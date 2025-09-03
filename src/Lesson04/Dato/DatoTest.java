b662790bd800979f223f54572cd7df29bb7c99f6
Esp <duckerlicious@duck.com>
2025-09-03 09:08:23 +0200

    Reevaluering

--- src/Lesson04/Dato/DatoTest.java
+++ src/Lesson04/Dato/DatoTest.java
@@ -5,24 +5,22 @@ import java.util.Scanner;
 public class DatoTest {
     public static void main(String[] args) {
 
-        Dato hackerMane = new Dato(2, 9, 2025);
+        Dato dagsDato = new Dato(2, 9, 2025);
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Er du Amerikaner (fat burger loving fuck)?(y/n) : ");
+        System.out.print("Er du Amerikaner? (y/n) : ");
         String nationalitet = input.next();
 
         if (nationalitet.equals("y")) {
-            System.out.println("Amerikansk dato: " + hackerMane.convert(false));
+            System.out.println("Amerikansk dato: " + dagsDato.convert(false));
         } else {
-            System.out.println("Europæisk dato: " + hackerMane.convert(true));
+            System.out.println("Europæisk dato: " + dagsDato.convert(true));
         }
 
 /*
         System.out.println("Amerikansk dato: " + hackerMane.convert(true) +
                 "\nEuropæisk dato: " + hackerMane.convert(false));
-                */
+*/
     }
 }
-
-
---
+++ src/Lesson04/Scanner04/Scanner04.java
@@ -0,0 +1,13 @@
+package Lesson04.Scanner04;
+
+import java.util.Scanner;
+
+public class Scanner04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String navn = sc.nextLine();
+
+        System.out.println(navn);
+    }
+
+}
